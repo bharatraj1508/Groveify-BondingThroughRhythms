6bf704b6e5f5c22c4a8822f8871a83dfa47a69d7
Karan Punjabi <karanpunjabi@Karans-Air.ht.home>
2023-08-23 13:03:34 -0400

    Generate Artist ID by searching name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './dashboard/profile/profile.component';
 import { AppComponent } from './app.component';
-import { AlbumsComponent } from './pages/albums/albums.component';
+import { AlbumsComponent } from './dashboard/albums/albums.component';
+import { ArtistsComponent } from './dashboard/artists/artists.component';
 
 const routes: Routes = [
   { path: 'landing-page', component: AppComponent },
   { path: 'user-info', component: ProfileComponent },
-  { path: 'albums', component: AlbumsComponent}
+  { path: 'albums', component: AlbumsComponent},
+  { path: 'artists', component: ArtistsComponent}
 ];
 
 @NgModule({
